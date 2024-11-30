59a5362c47604de1b94fdfcf28d11584e9e8343a
uxtarun <uxtarun@gmail.com>
2024-11-30 21:18:33 +0100

    Updated Obstacle Spawning

--- Assets/Scripts/GroundSpawner.cs
+++ Assets/Scripts/GroundSpawner.cs
@@ -3,11 +3,10 @@ using UnityEngine;
 
 public class GroundSpawner : MonoBehaviour
 {
-    public GameObject[] groundTiles;
+    public GameObject groundTile;
     public GameObject nextSapwnPoint;
 
 
-    private GameObject groundTile;
     //public void SpawnTile()
     //{
     //    GameObject temp = Instantiate(groundTile, nextSapwnPoint, Quaternion.identity);
@@ -16,8 +15,6 @@ public class GroundSpawner : MonoBehaviour
 
     public void SpawnNewTile()
     {
-        int randomIndex = Random.Range(0, groundTiles.Length);
-        GameObject groundTile = groundTiles[randomIndex];
         GameObject temp = Instantiate(groundTile, nextSapwnPoint.transform.position, Quaternion.identity);
     }
     // Start is called before the first frame update
--- Assets/Scripts/GroundTile.cs
+++ Assets/Scripts/GroundTile.cs
@@ -6,12 +6,16 @@ public class GroundTile : MonoBehaviour
 
     [SerializeField]
     private GroundSpawner groundSpawner;
+    public GameObject[] obstaclePrefabs; // Array to hold multiple obstacle prefabs
+    public float noObstacleChance = 0.3f; // Probability of no obstacle (e.g., 30%)
+
 
 
     // Start is called before the first frame update
     void Start()
     {
         groundSpawner = GameObject.FindObjectOfType<GroundSpawner>();
+        SpawnObstacles();
     }
 
     private void OnTriggerExit(Collider other)
@@ -23,5 +27,25 @@ public class GroundTile : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public GameObject obstaclePrefab;
+
+    void SpawnObstacles()
+    {
+        // Decide whether to spawn an obstacle or not
+        float noObstacleChance = 0.3f; // 30% chance of no obstacle
+        if (Random.Range(0f, 1f) < noObstacleChance)
+        {
+            // No obstacle will spawn
+            return;
+        }
+
+        // Choose a random spawn position
+        int obstacleSpawnIndex = Random.Range(2, 5); // Assuming valid child indices are 2 to 4
+        Transform spawnPoint = transform.GetChild(obstacleSpawnIndex).transform;
+
+        // Spawn the obstacle at the selected position
+        Instantiate(obstaclePrefab, spawnPoint.position, Quaternion.identity, transform);
     }
 }
